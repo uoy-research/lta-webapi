076e267f2f2262e95718d4aafd729e15dc9be268
Viktor Czyzewski <viktor.czyzewski@york.ac.uk>
2023-11-20 10:19:29 +0000

    Updated deploy script to have a dynamic server hostname

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 
 name='uoy/lta-api'
 file='./docker-lta-api-latest.tar'
+remote='langtrackappdev.york.ac.uk'
 
 sudo docker build -t "$name" .
 sudo docker save -o "$file" "$name"
 sudo chown "$USER:$USER" "$file"
 sudo docker load -i "$file"
-rsync -rva docker-lta-api-latest.tar langtrackappdev.york.ac.uk:~/langtrackapp/src/docker-lta-api-latest.tar
+rsync -rva docker-lta-api-latest.tar "$remote:~/docker-lta-api-latest.tar"
